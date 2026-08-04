7149f488d4ae1b2649b5af460e9e72188557afdb
CDOM201 <carlos@carlosdaniel.info>
2026-08-03 18:29:25 -0700

    chore(dimadocs): ignore generated Astro artifacts
    Agent: GPT-5.6-terra via opencode

--- dimadocs/.astro/content-assets.mjs
+++
@@ -1,5 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_Zm361X from "../../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx";
-import __ASTRO_IMAGE_IMPORT_jbkaP from "../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Fes%2Findex.mdx", __ASTRO_IMAGE_IMPORT_Zm361X], ["../../assets/logo.svg?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_jbkaP]]);
-		
